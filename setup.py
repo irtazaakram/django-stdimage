6a3a7b990a5ce780cc58bd220cc065ba04bfa542
irtazaakram <irtaza.akram@arbisoft.com>
2023-07-10 17:05:17 +0500

    fix: directory path

--- setup.py
+++ setup.py
@@ -26,6 +26,17 @@ class compile_translations(Command):
         pattern = "stdimage/locale/*/LC_MESSAGES/django.po"
         for file in glob.glob(pattern):
             name, ext = os.path.splitext(file)
+
+            # Create the directory
+            subprocess.check_call(
+                ["mkdir", "-p", f"{self.build_lib}/{os.path.dirname(name)}"],
+                cwd=BASE_DIR,
+            )
+            subprocess.check_call(
+                ["touch", f"{self.build_lib}/{name}.mo"],
+                cwd=BASE_DIR,
+            )
+
             cmd = ["msgfmt", "-c", "-o", f"{self.build_lib}/{name}.mo", file]
             self.announce(
                 "running command: %s" % " ".join(cmd), level=distutils.log.INFO
